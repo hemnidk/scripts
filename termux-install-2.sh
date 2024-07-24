c8ae24da912423f36b7ef614ecd3edb2a11482c8
hemnidk <hemnidk@gmail.com>
2024-07-24 13:05:52 +0300

    Update termux-install-2.sh

--- termux-install-2.sh
+++ termux-install-2.sh
@@ -2,5 +2,5 @@
 
 RED='\033[0;31m'
 sv-enable crond
-echo '* * * * * cd hemnid/hamster && npm run exec' | crontab -
+echo '0 * * * * cd hemnid/hamster && npm run exec' | crontab -
 echo "${RED}ENJOY\!\!\!"
